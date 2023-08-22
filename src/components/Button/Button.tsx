cd43ccf060da5c6a7dd6badd69ec1daeb6b4f1b6
Ricardo Alves <ricardospalves@gmail.com>
2023-08-22 18:19:59 -0300

    add loading prop to Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,14 @@
 import classNames from 'classnames'
 import { ComponentPropsWithoutRef, ElementType } from 'react'
+import { AiOutlineLoading3Quarters as LoadingIcon } from 'react-icons/ai'
 
 type AsProp<E extends ElementType> = {
   as: E
 }
 
-type ButtonOwnProps<E extends ElementType> = AsProp<E>
+type ButtonOwnProps<E extends ElementType> = AsProp<E> & {
+  loading?: boolean
+}
 
 type NativeProps<E extends ElementType> = ComponentPropsWithoutRef<E>
 
@@ -21,19 +24,32 @@ export const Button = <E extends ElementType>({
   as,
   className,
   children,
+  loading,
+  disabled,
   ...props
 }: ButtonProps<E>) => {
   const Component: ElementType = as
+  const isDisabled = disabled || loading
 
   return (
     <Component
       className={classNames(
         'flex items-center justify-center w-full h-12 text-sm uppercase font-bold text-white bg-purple-700 hover:bg-purple-950 focus-visible:bg-purple-950 transition-colors',
+        isDisabled &&
+          'opacity-70 select-none cursor-not-allowed pointer-events-none',
         className,
       )}
+      disabled={isDisabled}
       {...props}
     >
-      {children}
+      {loading ? (
+        <LoadingIcon
+          className="block w-8 h-8 fill-current animate-spin"
+          aria-hidden={true}
+        />
+      ) : (
+        children
+      )}
     </Component>
   )
 }
