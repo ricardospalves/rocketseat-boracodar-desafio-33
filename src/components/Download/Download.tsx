8915e562846670103d1d09ba5a61ebd302dc4ba6
Ricardo Alves <ricardospalves@gmail.com>
2023-08-22 18:08:55 -0300

    add icons

--- src/components/Download/Download.tsx
+++ src/components/Download/Download.tsx
@@ -1,4 +1,5 @@
 import { useTicket } from '@/hook/useTicket'
+import { BsCheck2Circle as CheckIcon } from 'react-icons/bs'
 import { Button } from '../Button/Button'
 
 export const Download = () => {
@@ -6,7 +7,14 @@ export const Download = () => {
 
   return (
     <>
-      <h2 className="text-lg pb-2 uppercase">Ticket gerado com sucesso</h2>
+      <h2 className="flex items-center gap-4 text-lg mb-8 uppercase">
+        <CheckIcon
+          className="block w-8 h-8 shrink-0 fill-green-500"
+          aria-hidden={true}
+        />
+
+        <span className="block grow">Ticket gerado com sucesso</span>
+      </h2>
 
       <Button as="a" href={ticketUrl} download>
         Fazer download
--- src/components/Form/UsernameFieldText/UsernameFieldText.tsx
+++ src/components/Form/UsernameFieldText/UsernameFieldText.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
 import { ComponentPropsWithoutRef, forwardRef } from 'react'
+import { SiGithub as GitHubIcon } from 'react-icons/si'
 
 type NativePropsToOmit = 'type'
 
@@ -17,17 +18,24 @@ export const UsernameFieldText = forwardRef<
   UsernameFieldTextProps
 >(({ error, className, ...props }, ref) => {
   return (
-    <input
-      type="text"
-      className={classNames([
-        'block w-full h-12 px-4 bg-white text-black border border-transparent focus:outline-none focus:ring ring-purple-700',
-        error ? 'border-red-400' : 'focus:border-purple-700',
-        className,
-      ])}
-      ref={ref}
-      placeholder="Exemplo: fulanodetal"
-      {...props}
-    />
+    <div className="relative">
+      <GitHubIcon
+        className="block w-5 h-5 my-auto absolute inset-y-0 left-2 fill-black"
+        aria-hidden={true}
+      />
+
+      <input
+        type="text"
+        className={classNames([
+          'block w-full h-12 pl-9 pr-4 bg-white text-black border border-transparent focus:outline-none focus:ring ring-purple-700',
+          error ? 'border-red-400' : 'focus:border-purple-700',
+          className,
+        ])}
+        ref={ref}
+        placeholder="Exemplo: fulanodetal"
+        {...props}
+      />
+    </div>
   )
 })
 
