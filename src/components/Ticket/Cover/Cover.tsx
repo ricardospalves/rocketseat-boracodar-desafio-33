9b757edf9ff0a0d6cd3c32278a9ae382131caee3
Ricardo Alves <ricardospalves@gmail.com>
2023-08-23 09:36:49 -0300

    fix image src attribute in production

--- src/components/Ticket/Cover/Cover.tsx
+++ src/components/Ticket/Cover/Cover.tsx
@@ -1,8 +1,17 @@
+import { APP_CONFIG } from '@/constants/appConfig/appConfig'
+
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+const IMAGE_PATH = 'images/ticket-cover.svg'
+
 export const Cover = () => {
   return (
     <div className="col-span-7 bg-purple-700">
       <img
-        src="./images/ticket-cover.svg"
+        src={
+          IS_PRODUCTION
+            ? `${APP_CONFIG.websiteUrl}/${IMAGE_PATH}`
+            : `./${IMAGE_PATH}`
+        }
         className="block w-full h-full"
         alt="IA para Devs"
       />
